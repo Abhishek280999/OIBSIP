1e76b3ba4352b4a6352a09f21a9e83162cb7bf30
Abhishek Darwekar <117111036+Abhishek280999@users.noreply.github.com>
2023-03-17 15:03:35 +0530

    task 1

---
+++ Landing page/landing_page.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+    $('.navbar-collapse ul li a').click(function(){
+    $('.navbar-toggle:visible').click();
+      }); 
+    
+   $('a[href*="#"]:not([href="#"])').click((e) => {
+      const target = $(e.target.hash);
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+  
+        return false;
+      }
+    });
+  });
